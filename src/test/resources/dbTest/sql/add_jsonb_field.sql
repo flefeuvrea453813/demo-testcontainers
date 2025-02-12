ad044dcb466d7e22b2f8b5d893e8398b1d873835
flefeuvrea453813 <florian.lefeuvre@eviden.com>
2025-02-12 08:25:10 +0100

    change conf liquibase for test

--- src/test/resources/dbTest/sql/add_jsonb_field.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE athlete ADD COLUMN address jsonb DEFAULT '{}'::jsonb;
-
-UPDATE athlete SET address = '{"numero": 10,"rue": "intendance","code_postal":"33000", "ville":"BORDEAUX"}'::jsonb WHERE firstname = 'Camille' ;
